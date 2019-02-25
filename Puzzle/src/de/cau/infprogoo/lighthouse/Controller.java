93328009d4b9d435462c66fff52cdc42efb39f02
olegattow <olegattow@192.168.242.88>
2019-02-25 14:50:57 +0100

    Klassen für MVC hinzugefügt

---
+++ Puzzle/src/de/cau/infprogoo/lighthouse/Controller.java
@@ -0,0 +1,10 @@
+package de.cau.infprogoo.lighthouse;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Puzzle/src/de/cau/infprogoo/lighthouse/View.java
@@ -0,0 +1,10 @@
+package de.cau.infprogoo.lighthouse;
+
+public class View {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
