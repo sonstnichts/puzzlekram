cb50ef61f8820b73bcd7aec57a6e663a564af7f7
olegattow <olegattow@192.168.246.151>
2019-02-26 18:27:16 +0100

    Array für Spiel erstellt und Feldgröße bestimmt

--- Puzzle/src/de/cau/infprogoo/lighthouse/Model.java
+++ Puzzle/src/de/cau/infprogoo/lighthouse/Model.java
@@ -2,6 +2,10 @@ package de.cau.infprogoo.lighthouse;
 
 public class Model {
 
+int[][] spielfeld = new int [4][6];
+public final static int WIDTH = 400;
+public final static int HEIGHT = 600;
+
 
 
 }
