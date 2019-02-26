a9de5f4dc5ab474aed86533a2a683ff11130bb97
olegattow <olegattow@me.com>
2019-02-26 23:26:52 +0100

    Zweidimensionales Array mit Player-Position füllen noch nicht fertig

--- Puzzle/src/de/cau/infprogoo/lighthouse/Model.java
+++ Puzzle/src/de/cau/infprogoo/lighthouse/Model.java
@@ -5,8 +5,6 @@ import java.util.Set;
 
 
 
-
-
 /**
  * This class is our Model. It contains the skeletal structure of our game.
  * It represents the current state of the game.
@@ -18,17 +16,13 @@ public class Model {
 int[][] spielfeld = new int [4][6];
 private final int WIDTH;
 private final int HEIGHT;
+//Position of the player
+private int player1 = spielfeld [0][0];
+private int player2 = spielfeld [0][2];
+private int player3 = spielfeld [3][0];
+private int player4 = spielfeld [3][2];
+private int player5 = spielfeld [1][1];
 
-private int player1X;
-private int player1Y;
-private int player2X;
-private int player2Y;
-private int player3X;
-private int player3Y;
-private int player4X;
-private int player4Y;
-private int player5X;
-private int player5Y;
 
 /** Set of views registered to be notified of world updates. */
 private final Set<GView> views = new HashSet<>();
@@ -40,6 +34,7 @@ public Model(int width, int height) {
 	this.HEIGHT = height;
 }
 
+
 public int getWIDTH() {
 	return WIDTH;
 }
@@ -49,109 +44,65 @@ public int getHEIGHT() {
 	return HEIGHT;
 }
 
-public int getPlayer1X() {
-	return player1X;
+
+public int getPlayer1() {
+	return player1;
 }
 
 
-public void setPlayer1X(int player1x) {
-	player1X = player1x;
+public void setPlayer1(int player1) {
+	this.player1 = player1;
+	updateViews();
 }
 
 
-public int getPlayer1Y() {
-	return player1Y;
+public int getPlayer2() {
+	return player2;
 }
 
 
-public void setPlayer1Y(int player1y) {
-	player1Y = player1y;
+public void setPlayer2(int player2) {
+	this.player2 = player2;
+	updateViews();
 }
 
 
-public int getPlayer2X() {
-	return player2X;
+public int getPlayer3() {
+	return player3;
 }
 
 
-public void setPlayer2X(int player2x) {
-	player2X = player2x;
+public void setPlayer3(int player3) {
+	this.player3 = player3;
+	updateViews();
+	
 }
 
 
-public int getPlayer2Y() {
-	return player2Y;
+public int getPlayer4() {
+	return player4;
 }
 
 
-public void setPlayer2Y(int player2y) {
-	player2Y = player2y;
+public void setPlayer4(int player4) {
+	this.player4 = player4;
+	updateViews();
 }
 
 
-public int getPlayer3X() {
-	return player3X;
+public int getPlayer5() {
+	return player5;
 }
 
 
-public void setPlayer3X(int player3x) {
-	player3X = player3x;
-}
-
-
-public int getPlayer3Y() {
-	return player3Y;
-}
-
-
-public void setPlayer3Y(int player3y) {
-	player3Y = player3y;
-}
-
-
-public int getPlayer4X() {
-	return player4X;
-}
-
-
-public void setPlayer4X(int player4x) {
-	player4X = player4x;
-}
-
-
-public int getPlayer4Y() {
-	return player4Y;
-}
-
-
-public void setPlayer4Y(int player4y) {
-	player4Y = player4y;
-}
-
-
-public int getPlayer5X() {
-	return player5X;
-}
-
-
-public void setPlayer5X(int player5x) {
-	player5X = player5x;
-}
-
-
-public int getPlayer5Y() {
-	return player5Y;
-}
-
-
-public void setPlayer5Y(int player5y) {
-	player5Y = player5y;
+public void setPlayer5(int player5) {
+	this.player5 = player5;
+	updateViews();
 }
 
 
 
-///////////////////////////////////////////////////////////////////////////
-// View Management
+
 
 /**
 * Adds the given view of the world and updates it once. Once registered through
@@ -172,6 +123,10 @@ for (GView view : views) {
 view.update(this);
 }
 }
+
+
+
+
 
 
 
