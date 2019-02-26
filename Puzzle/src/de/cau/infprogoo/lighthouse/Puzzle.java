e0c66a21124bddcba015806c378f550de6b110d7
olegattow <olegattow@me.com>
2019-02-26 23:25:47 +0100

    Spielstarter

---
+++ Puzzle/src/de/cau/infprogoo/lighthouse/Puzzle.java
@@ -0,0 +1,25 @@
+package de.cau.infprogoo.lighthouse;
+
+import java.awt.Color;
+
+import acm.program.GraphicsProgram;
+
+
+public class Puzzle extends GraphicsProgram{
+	@Override
+	public void init() {
+		this.setBackground(Color.LIGHT_GRAY);
+		setSize(500,700);
+		// Create the world
+		Model world = new Model(4, 6);
+
+		// Create two views and let the world know about them
+		GView gview = new GView();
+		add(gview);
+		world.registerView(gview);
+
+		
+	}
+
+
+}
