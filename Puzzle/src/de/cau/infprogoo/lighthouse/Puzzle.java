81fb32f341a8a41ebbef32bdf7f0e45624b18764
Ole Gattow <olegattow@me.com>
2019-03-06 14:15:04 +0100

    Lighthouse hinzugefügt

--- Puzzle/src/de/cau/infprogoo/lighthouse/Puzzle.java
+++ Puzzle/src/de/cau/infprogoo/lighthouse/Puzzle.java
@@ -1,7 +1,7 @@
 package de.cau.infprogoo.lighthouse;
 
 import java.awt.Color;
-
+import java.io.IOException;
 
 import acm.program.GraphicsProgram;
 
@@ -12,22 +12,26 @@ public class Puzzle extends GraphicsProgram{
 		this.setBackground(Color.GRAY);
 		setSize(500,700);
 		// Create the world
-		Model world = new Model(4, 6);
+		Model model = new Model(4, 6);
 
 		// Create two views and let the world know about them
 		GView gview = new GView();
 		add(gview);
-		world.registerView(gview);
+		model.registerView(gview);
+	
 		
 		
-		LighthouseDisplay display = new LighthouseDisplay("die_dicke_KATZE", "API-TOK_ySnj-ST3X-GqJm-mprn-Pt9o");
-
+		LighthouseView display = new LighthouseView("die_dicke_KATZE", "API-TOK_Aem9-K+La-ibUC-H41Q-Ub2o");
+		display.connect();
+		model.registerView(display);
 		
 	
-		new GController(world, this);
+		new GController(model, this);
 
 		
 	}
+
+	
 
 
 }
